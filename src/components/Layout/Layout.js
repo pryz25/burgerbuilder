38d0a8379b506cf1940724cee07e702f5fe5093a
Peter R <pryzanych@gmail.com>
2020-09-10 13:16:54 -0700

    Fix CSS Module naming conventions

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-import './Layout.css';
+import classes from'./Layout.module.css';
 
 const layout = ( props ) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className="Content">
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
