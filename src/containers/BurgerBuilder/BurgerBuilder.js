e16c3a516447d7c9507e76ba2242dc9afb18ebac
Peter R <pryzanych@gmail.com>
2020-09-24 13:37:28 -0700

    Wire functionality to add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDEINT_PRICES = {
+  salad: .5,
+  cheese: .4,
+  meat: 1.3,
+  bacon: .7,
+}
+
 class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
@@ -13,15 +20,37 @@ class BurgerBuilder extends Component {
         bacon: 1,
         cheese: 2,
         meat: 2,
-      }
+      },
+      totalPrice: 4,
     };
+    this.addIngredientHandler = this.addIngredientHandler.bind(this);
+    this.removeIngredientHandler = this.removeIngredientHandler.bind(this);
   }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updateCount = oldCount + 1;
+    const updateIngredients = {
+      ...this.state.ingredients
+    };
+    updateIngredients[type] = updateCount;
+    const priceAddition = INGREDEINT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+  }
+
+  removeIngredientHandler = (type) => {
+
+  };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler} />
       </Aux>
     );
   }
