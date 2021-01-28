a52e0a4d1ebf392ed1756f8ca6007b66d701776e
Peter R <pryzanych@gmail.com>
2021-01-28 10:28:42 -0800

    Refactor SideDrawer and Layout to pass props for opening and closing SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,13 +6,20 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
   render () {
     return (
     <Aux>
       <Toolbar />
-      <SideDrawer />
+      <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
       <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
       </main>
     </Aux>
     )
