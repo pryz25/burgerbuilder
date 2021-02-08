ae8265f66cbf2eb90d55f1049454dd85475c4a5f
Peter R <pryzanych@gmail.com>
2021-02-08 15:25:35 -0800

    Build dummy post to Firebase in checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDEINT_PRICES = {
   salad: .5,
@@ -35,7 +36,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('Continue!');
+    // alert('Continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Person Human',
+        address: {
+          street: 'That Drive',
+          zipCode: '00000',
+          country: 'USA'
+        },
+        email: 'fake@domain.edu'
+      },
+      deliveryMethod: 'fast',
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   updatePurchaseState = (ingredients) => {
