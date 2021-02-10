71f46ebf3e90687434cbf57c93ce141996a6bc14
Peter R <pryzanych@gmail.com>
2021-02-10 14:59:51 -0800

    Create error handler HoC

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Aux/Aux';
+
+const withErrorHandler =(WrappedComponent) => {
+  return (props) => {
+    return (
+      <Aux>
+        <Modal>
+          Something didn't work...
+          </Modal>
+        <WrappedComponent {...props} />
+      </Aux>
+    );
+  }
+}
